cc243865b8553e7ac69d9f78f0a86e3b22e9f2f7
Sme <esahig15789@gmail.com>
2019-02-14 22:58:28 -0500

    Initial commit

---
+++ MAKE HACK_debug.cmd
@@ -0,0 +1,32 @@
+cd %~dp0
+
+copy FE8_clean.gba _MekkahRestrictedHackComp1.gba
+
+echo: | (AssembleMusic.cmd)
+
+cd "%~dp0Tables"
+
+echo: | (c2ea "%~dp0FE8_clean.gba")
+
+cd "%~dp0Text"
+
+echo: | (text-process-classic text_buildfile.txt --parser-exe "%~dp0Event Assembler/Tools/ParseFile.exe")
+
+cd "%~dp0Maps"
+
+echo: | (tmx2ea -s)
+
+cd "%~dp0Palettes"
+
+echo: | (pal2ea "%~dp0Palettes\Palettes.txt")
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0_MekkahRestrictedHackComp1.gba" "-input:%~dp0Debug.event"
+
+if exist "%~dp0ups/ups.exe" (
+    cd "%~dp0ups"
+    ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0_MekkahRestrictedHackComp1.gba" -o "%~dp0_MekkahRestrictedHackComp1.ups"
+)
+
+pause
